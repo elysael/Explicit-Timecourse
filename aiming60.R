ac5b4cf5cbfcd1e0241301e2ccc173101db53893
Elysa Eliopulos <elysa@elysas-air.home>
2025-03-27 19:20:30 -0400

    Learned a code to calculate the CI and add shaded region that represents a 95% CI for the data on the graph.

--- aiming60.R
+++ aiming60.R
@@ -21,12 +21,12 @@ combinedaimdev <- do.call(cbind,all_aim_deviations)
 average_aim_deviation <- apply(combinedaimdev, 1, mean, na.rm = TRUE)
 
 
-plot(average_aim_deviation, type = "l", col = "skyblue3", lwd = 2, xlim = c(0, 256), ylim = c(0, 70),
+plot(average_aim_deviation, type = "l", col = "firebrick2", lwd = 2, xlim = c(0, 256), ylim = c(-30, 60),
      main = "Average Aim Deviation with a 60 Degree Perturbation", xlab = "Trial", ylab = "Aim Deviation (degrees)")
-smooth_line<- smooth.spline(average_aim_deviation)
-lines(smooth_line, col = "black", lwd = 2)
+#smooth_line<- smooth.spline(average_aim_deviation)
+#lines(smooth_line, col = "black", lwd = 1)
 
-abline (h=60, col="black", lwd = 1.5, lty=3)
+abline (h=0, col="black", lwd = 2, lty=3)
 
 
 #each participant
@@ -53,33 +53,72 @@ combinedaimdev <- do.call(cbind,all_aim_deviations)
 
 
 # Plot one participant to see trial by trial
-plot(cleaned_aim[[6]], , col = "orchid4", type = "p", lwd = 2,
-     xlim = c(0, 256), ylim = c(0, 80),
+plot(cleaned_aim[[1]], , col = "deeppink", type = "p", lwd = 2,
+     xlim = c(0, 256), ylim = c(-20, 60),
      main = "Aim Deviation with a 60 Degree Perturbation",
      xlab = "Trial", ylab = "Aim Strategy (degrees)")
 
-abline (h=60, col="black", lwd = 1.5, lty=3)
+abline (h=0, col="black", lwd = 1.5, lty=3)
 
 
-#8, 6 has really messy data in beginning
-#13 too 
+#8, 6, 13 has really messy data in beginning.. not sure how to go about cleaning that at the moment.
 
 
 
 #compare reach and aim dev
-plot(average_aim_deviation, type = "n", col = "blue", lwd = 2, 
-     xlim = c(0, 256), ylim = c(0, 70), 
-     main = "Comparison of Aim Deviation for Two Conditions", 
-     xlab = "Trial", ylab = "Aim Deviation (degrees)")
-
-#lines(average_reach_deviation, col = "hotpink", lwd = 2)
-
-
-#we can "smooth out the lines just to compare easier"
-smooth_line1 <- smooth.spline(average_aim_deviation)
-lines(smooth_line1, col = "darkorange1", lwd = 3)
-smooth_line2 <- smooth.spline(average_reach_deviation)
-lines(smooth_line2, col = "darkmagenta", lwd = 3)
-legend("topright", legend = c("Aim Deviation", "Reach Deviation"), col = c("darkorange1", "darkmagenta"), lwd = 2)
+#plot(average_aim_deviation, type = "l", col = "magenta2", lwd = 2, 
+    # xlim = c(0, 256), ylim = c(-20, 70), 
+   #  main = "Averages for Aim and Reach Deviation among Participants with a 60 Degree Pertuburbation", 
+     #xlab = "Trial", ylab = "Aim Deviation (degrees)")
+#lines(average_reach_deviation, col = "black", lwd = 2)
+#legend("bottomright", legend = c("Aim Deviation", "Reach Deviation"), col = c("magenta2", "black"), lwd = 2)
 
 
+
+#we can smooth out the data and add a sahded region that covers the CI 
+smooth_line1 <- smooth.spline(average_aim_deviation, spar = 0.32)
+lines(smooth_line1, col = "magenta2", lwd = 3)
+smooth_line2 <- smooth.spline(average_reach_deviation, spar = 0.32)
+lines(smooth_line2, col = "black", lwd = 3)
+legend("topright", legend = c("Aim Deviation", "Reach Deviation"), col = c("magenta2", "black"), lwd = 2)
+
+abline (h=0, col="black", lwd = 1.5, lty=3)
+
+#find se for ci intervals
+se_aim <- sd(average_aim_deviation) / sqrt(length(average_aim_deviation))
+upper_aim <- average_aim_deviation + 1.96 * se_aim
+lower_aim <- average_aim_deviation - 1.96 * se_aim
+
+se_reach <- sd(average_reach_deviation) / sqrt(length(average_reach_deviation))
+upper_reach <- average_reach_deviation + 1.96 * se_reach
+lower_reach <- average_reach_deviation - 1.96 * se_reach
+
+#"smooth" the data points
+smooth_line1 <- smooth.spline(average_aim_deviation, spar = 0.32) #lower spar = less smooth
+smooth_line2 <- smooth.spline(average_reach_deviation, spar = 0.32)
+smooth_upper_aim <- smooth.spline(upper_aim, spar = 0.32)
+smooth_lower_aim <- smooth.spline(lower_aim, spar = 0.32)
+smooth_upper_reach <- smooth.spline(upper_reach, spar = 0.32)
+smooth_lower_reach <- smooth.spline(lower_reach, spar = 0.32)
+
+plot(1:length(average_aim_deviation), average_aim_deviation, type = "n", 
+     xlim = c(0, length(average_aim_deviation)), 
+     ylim = range(c(lower_aim, upper_aim, lower_reach, upper_reach)), 
+     main = "Averages for Aim and Reach Deviation", 
+     xlab = "Trial", ylab = "Deviation (degrees)")
+
+
+polygon(c(smooth_upper_aim$x, rev(smooth_lower_aim$x)), 
+        c(smooth_upper_aim$y, rev(smooth_lower_aim$y)), 
+        col = rgb(1, 0, 1, alpha = 0.26), border = NA) #lower alpha = more transparent
+
+polygon(c(smooth_upper_reach$x, rev(smooth_lower_reach$x)), 
+        c(smooth_upper_reach$y, rev(smooth_lower_reach$y)), 
+        col = rgb(0, 0, 0, alpha = 0.26), border = NA)
+
+#plot
+lines(smooth_line1, col = "magenta2", lwd = 3)
+lines(smooth_line2, col = "black", lwd = 3)
+legend("topright", legend = c("Aim Deviation", "Reach Deviation"), 
+       col = c("magenta2", "black"), lwd = 2)
+abline(h = 0, col = "black", lwd = 1.5, lty = 3)
