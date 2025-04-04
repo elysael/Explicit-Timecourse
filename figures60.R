581ae1d91aadfa551b80a6854e5c1dfc13598e81
Elysa Eliopulos <elysa@elysas-air.home>
2025-04-03 23:58:22 -0400

    Specialized 60 degree names

--- figures60.R
+++ figures60.R
@@ -1,5 +1,5 @@
 plotAligned60 <- function() {
-  aligned_data <- getAligned()
+  aligned60_data <- getAligned60()
   
   group1_c <- do.call(rbind, aligned_data$group1)
   group2_c <- do.call(rbind, aligned_data$group2)
@@ -20,7 +20,7 @@ abline(h=0, col="black", lty=3)
 
 plotRotated60 <- function () {
   
-  rotated_data <- getRotated()
+  rotated60_data <- getRotated60()
   
   group1_combined <- do.call(rbind, group1_rotated)
   group2_combined <- do.call(rbind, group2_rotated)
@@ -45,7 +45,7 @@ plotRotated60 <- function () {
 
 plotAfter60 <- function () {
   
-  after_data <- getAfter()
+  after60_data <- getAfter60()
   
   g1_combined <- do.call(rbind, group1_after)
   g2_combined <- do.call(rbind, group2_after)
@@ -69,32 +69,32 @@ plotAfter60 <- function () {
 
 plotAll60 <- function() {
   
-  aligned_data <- getAligned()
-  rotated_data <- getRotated()
-  after_data <- getAfter()
+  aligned60_data <- getAligned60()
+  rotated60_data <- getRotated60()
+  after60_data <- getAfter60()
   
 
-  group1_c <- do.call(rbind, aligned_data$group1)
-  group2_c <- do.call(rbind, aligned_data$group2)
-  group1_combined <- do.call(rbind, group1_rotated)
-  group2_combined <- do.call(rbind, group2_rotated)
-  g1_combined <- do.call(rbind, group1_after)
-  g2_combined <- do.call(rbind, group2_after)
+  group1_c60 <- do.call(rbind, aligned60_data$group1)
+  group2_c60 <- do.call(rbind, aligned60_data$group2)
+  group1_combined60 <- do.call(rbind, rotated60_data$group1)
+  group2_combined60 <- do.call(rbind, rotated60_data$group2)
+  g1_combined60 <- do.call(rbind, after60_data$group1)
+  g2_combined60 <- do.call(rbind, after60_data$group2)
   
-  group1avg <- aggregate(group1_c$reachdeviation_deg, by = list(group1_c$cutrial_no), FUN = mean)
-  group2avg <- aggregate(group2_c$reachdeviation_deg, by = list(group2_c$cutrial_no), FUN = mean)
-  group1_avg <- aggregate(group1_combined$reachdeviation_deg, by = list(group1_combined$cutrial_no), FUN = mean)
-  group2_avg <- aggregate(group2_combined$reachdeviation_deg, by = list(group2_combined$cutrial_no), FUN = mean)
-  g1_avg <- aggregate(g1_combined$reachdeviation_deg, by = list(g1_combined$cutrial_no), FUN = mean)
-  g2_avg <- aggregate(g2_combined$reachdeviation_deg, by = list(g2_combined$cutrial_no), FUN = mean)
+  group1avg60 <- aggregate(group1_c60$reachdeviation_deg, by = list(group1_c60$cutrial_no), FUN = mean)
+  group2avg60 <- aggregate(group2_c60$reachdeviation_deg, by = list(group2_c60$cutrial_no), FUN = mean)
+  group1_avg60 <- aggregate(group1_combined60$reachdeviation_deg, by = list(group1_combined60$cutrial_no), FUN = mean)
+  group2_avg60 <- aggregate(group2_combined60$reachdeviation_deg, by = list(group2_combined60$cutrial_no), FUN = mean)
+  g1_avg60 <- aggregate(g1_combined60$reachdeviation_deg, by = list(g1_combined60$cutrial_no), FUN = mean)
+  g2_avg60 <- aggregate(g2_combined60$reachdeviation_deg, by = list(g2_combined60$cutrial_no), FUN = mean)
   
   
-all_data <- rbind(
-  group1avg, group2avg, 
-  group1_avg, group2_avg, 
-  g1_avg ,  g2_avg)
+all_data60 <- rbind(
+  group1avg60, group2avg60, 
+  group1_avg60, group2_avg60, 
+  g1_avg60 ,  g2_avg60)
 
-all_data <- all_data[order(all_data$Group.1), ] 
+all_data60 <- all_data60[order(all_data60$Group.1), ] 
   
 #Get CI
 CI <- function(df) {
@@ -110,34 +110,34 @@ CI(g2_combined)
 CI(group1_combined)
 CI(group2_combined)
 
-ci_list <- list(
-  CI(group1_c),
-  CI(group2_c),
-  CI(g1_combined),
-  CI(g2_combined),
-  CI(group1_combined),
-  CI(group2_combined)
+ci_list60 <- list(
+  CI(group1_c60),
+  CI(group2_c60),
+  CI(g1_combined60),
+  CI(g2_combined60),
+  CI(group1_combined60),
+  CI(group2_combined60)
 )
 
-ci_combined <- do.call(rbind, ci_list)
-ci_combined <- ci_combined[order(ci_combined$cutrial_no), ]
+ci_combined60 <- do.call(rbind, ci_list60)
+ci_combined60 <- ci_combined60[order(ci_combined60$cutrial_no), ]
 
-lo <- ci_combined$reachdeviation_deg[, 1]
-hi <- ci_combined$reachdeviation_deg[ ,2]
-cutrial_no <- ci_combined$cutrial_no
+lo60 <- ci_combined60$reachdeviation_deg[, 1]
+hi60 <- ci_combined60$reachdeviation_deg[ ,2]
+cutrial_no60 <- ci_combined60$cutrial_no
 
 
 y_lower_limit <- -20
 y_upper_limit <- 80
 
 
-valid_indices <- lo >= y_lower_limit & lo <= y_upper_limit & hi >= y_lower_limit & hi <= y_upper_limit
-lo_filtered <- lo[valid_indices]
-hi_filtered <- hi[valid_indices]
-cutrial_no_filtered <- cutrial_no[valid_indices]
+valid_indices60 <- lo60 >= y_lower_limit & lo60 <= y_upper_limit & hi60 >= y_lower_limit & hi60 <= y_upper_limit
+lo_filtered60 <- lo60[valid_indices60]
+hi_filtered60 <- hi60[valid_indices60]
+cutrial_no_filtered60 <- cutrial_no60[valid_indices60]
 
 
-if (length(lo_filtered) == length(hi_filtered) && length(hi_filtered) == length(cutrial_no_filtered)) {
+if (length(lo_filtered60) == length(hi_filtered60) && length(hi_filtered60) == length(cutrial_no_filtered60)) {
   plot(-1000, 1000, type = "n",
        main = "Reach Deviation Across All Trials", 
        xlab = "Trial", ylab = "Reach Deviation (degrees)",
@@ -145,16 +145,16 @@ if (length(lo_filtered) == length(hi_filtered) && length(hi_filtered) == length(
   
 
 #lines(all_data$Group.1, all_data$x, col = "hotpink", lwd = 2, pch = 16)  
-smoothreach <- smooth.spline(all_data$Group.1, all_data$x, spar = 0.02)
-lines(smoothreach, col='tomato', lwd=1)
+smoothreach60 <- smooth.spline(all_data60$Group.1, all_data60$x, spar = 0.02)
+lines(smoothreach60, col='tomato', lwd=2)
 abline(h = 0, col = "black", lty = 3)
 
-lo_smooth_filtered <- smooth.spline(cutrial_no_filtered, lo_filtered, spar = 0.008)
-hi_smooth_filtered <- smooth.spline(cutrial_no_filtered, hi_filtered, spar = 0.008)
+lo_smooth_filtered60 <- smooth.spline(cutrial_no_filtered60, lo_filtered60, spar = 0.008)
+hi_smooth_filtered60 <- smooth.spline(cutrial_no_filtered60, hi_filtered60, spar = 0.008)
 
 # Draw the polygon for the filtered CIs
-polygon(x = c(lo_smooth_filtered$x, rev(hi_smooth_filtered$x)),
-        y = c(lo_smooth_filtered$y, rev(hi_smooth_filtered$y)),
+polygon(x = c(lo_smooth_filtered60$x, rev(hi_smooth_filtered60$x)),
+        y = c(lo_smooth_filtered60$y, rev(hi_smooth_filtered60$y)),
         col = rgb(1, 0.388, 0.278, alpha = 0.2),
         border = NA)
 } else {
@@ -166,7 +166,7 @@ polygon(x = c(lo_smooth_filtered$x, rev(hi_smooth_filtered$x)),
 #####AIMING
 
 plotAlignedAim60 <- function() {
-  aligned_data <- getAligned()
+  aligned60_data <- getAligned60()
   
   group1_c <- do.call(rbind, aligned_data$group1)
   group2_c <- do.call(rbind, aligned_data$group2)
@@ -188,7 +188,7 @@ abline(h=0, col="black", lty=3)
 
 plotStrategy60 <- function () {
   
-  rotated_data <- getRotated()
+  rotated60_data <- getRotated60()
   
   group1_combined <- do.call(rbind, group1_rotated)
   group2_combined <- do.call(rbind, group2_rotated)
@@ -213,7 +213,7 @@ plotStrategy60 <- function () {
 
 plotAfterAim60 <- function () {
   
-  after_data <- getAfter()
+  after60_data <- getAfter60()
   
   g1_combined <- do.call(rbind, group1_after)
   g2_combined <- do.call(rbind, group2_after)
@@ -222,7 +222,6 @@ plotAfterAim60 <- function () {
   g2_avg <- aggregate(g2_combined$aimdeviation_deg, by = list(g2_combined$cutrial_no), FUN = mean)
   
   combined_d <- rbind(g1_avg, g2_avg)
-  combined_d <- combined_d[order(combined_d$Group.1), ] 
   
   
   plot(-1000, 1000, type = "n",
@@ -232,37 +231,39 @@ plotAfterAim60 <- function () {
   lines(combined_d$Group.1, combined_d$x, col = "deeppink", lwd = 2, pch = 16)  
   
   abline(h=0, col="black", lty=3)
+  abline(v=233, col="turquoise3", lty=3)
+  text(x = 233, y = 0, labels = "Left hand Trials for Group 1", col = "black", pos = 4, cex = 1)
 }
 
 
 plotAllAim60 <- function () {
   
-  aligned_data <- getAligned()
-  rotated_data <- getRotated()
-  after_data <- getAfter()
+  aligned60_data <- getAligned60()
+  rotated60_data <- getRotated60()
+  after60_data <- getAfter60()
   
   
-  group1_c <- do.call(rbind, aligned_data$group1)
-  group2_c <- do.call(rbind, aligned_data$group2)
-  group1_combined <- do.call(rbind, group1_rotated)
-  group2_combined <- do.call(rbind, group2_rotated)
-  g1_combined <- do.call(rbind, group1_after)
-  # g2_combined <- do.call(rbind, group2_after)
+  group1_c60 <- do.call(rbind, aligned60_data$group1)
+  group2_c60 <- do.call(rbind, aligned60_data$group2)
+  group1_combined60 <- do.call(rbind, rotated60_data$group1)
+  group2_combined60 <- do.call(rbind, rotated60_data$group2)
+  g1_combined60 <- do.call(rbind, after60_data$group1)
+  g2_combined60 <- do.call(rbind, after60_data$group2)
   
-  group1avg <- aggregate(group1_c$aimdeviation_deg, by = list(group1_c$cutrial_no), FUN = mean)
-  group2avg <- aggregate(group2_c$aimdeviation_deg, by = list(group2_c$cutrial_no), FUN = mean)
-  group1_avg <- aggregate(group1_combined$aimdeviation_deg, by = list(group1_combined$cutrial_no), FUN = mean)
-  group2_avg <- aggregate(group2_combined$aimdeviation_deg, by = list(group2_combined$cutrial_no), FUN = mean)
-  g1_avg <- aggregate(g1_combined$aimdeviation_deg, by = list(g1_combined$cutrial_no), FUN = mean)
-  g2_avg <- aggregate(g2_combined$aimdeviation_deg, by = list(g2_combined$cutrial_no), FUN = mean)
+  group1avg60 <- aggregate(group1_c60$aimdeviation_deg, by = list(group1_c60$cutrial_no), FUN = mean)
+  group2avg60 <- aggregate(group2_c60$aimdeviation_deg, by = list(group2_c60$cutrial_no), FUN = mean)
+  group1_avg60 <- aggregate(group1_combined60$aimdeviation_deg, by = list(group1_combined60$cutrial_no), FUN = mean)
+  group2_avg60 <- aggregate(group2_combined60$aimdeviation_deg, by = list(group2_combined60$cutrial_no), FUN = mean)
+  g1_avg60 <- aggregate(g1_combined60$aimdeviation_deg, by = list(g1_combined60$cutrial_no), FUN = mean)
+  g2_avg60 <- aggregate(g2_combined60$aimdeviation_deg, by = list(g2_combined60$cutrial_no), FUN = mean)
   
   
-  all_data <- rbind(
-    group1avg, group2avg, 
-    group1_avg, group2_avg, 
-    g1_avg ,  g2_avg)
+  all_data60 <- rbind(
+    group1avg60, group2avg60, 
+    group1_avg60, group2_avg60, 
+    g1_avg60 ,  g2_avg60)
   
-  all_data <- all_data[order(all_data$Group.1), ] 
+  all_data60 <- all_data60[order(all_data60$Group.1), ] 
   
   #Get CI
   CI <- function(df) {
@@ -274,66 +275,60 @@ plotAllAim60 <- function () {
   CI(group1_c)
   CI(group2_c)
   CI(g1_combined)
- # CI(g2_combined)
+  # CI(g2_combined)
   CI(group1_combined)
   CI(group2_combined)
   
-  ci_list <- list(
-    CI(group1_c),
-    CI(group2_c),
-    CI(g1_combined),
-    CI(group1_combined),
-    CI(group2_combined))
+  ci_list60 <- list(
+    CI(group1_c60),
+    CI(group2_c60),
+    CI(g1_combined60),
+    CI(group1_combined60),
+    CI(group2_combined60))
+    
   
-  ci_combined <- do.call(rbind, ci_list)
-  ci_combined <- ci_combined[order(ci_combined$cutrial_no), ]
+  ci_combined60 <- do.call(rbind, ci_list60)
+  ci_combined60 <- ci_combined60[order(ci_combined60$cutrial_no), ]
   
- 
-  lo <- ci_combined$aimdeviation_deg [, 1]
-  hi <- ci_combined$aimdeviation_deg[ ,2]
-  cutrial_no <- ci_combined$cutrial_no
+  
+  lo <- ci_combined60$aimdeviation_deg [, 1]
+  hi <- ci_combined60$aimdeviation_deg[ ,2]
+  cutrial_no <- ci_combined60$cutrial_no
   
   
   y_lower_limit <- -20
   y_upper_limit <- 80
   
   
-  valid_indices <- lo >= y_lower_limit & lo <= y_upper_limit & hi >= y_lower_limit & hi <= y_upper_limit
-  lo_filtered <- lo[valid_indices]
-  hi_filtered <- hi[valid_indices]
-  cutrial_no_filtered <- cutrial_no[valid_indices]
+  valid_indices60 <- lo >= y_lower_limit & lo <= y_upper_limit & hi >= y_lower_limit & hi <= y_upper_limit
+  lo_filtered60 <- lo[valid_indices60]
+  hi_filtered60 <- hi[valid_indices60]
+  cutrial_no_filtered60 <- cutrial_no[valid_indices60]
   
   
-  if (length(lo_filtered) == length(hi_filtered) && length(hi_filtered) == length(cutrial_no_filtered)) {
+  if (length(lo_filtered60) == length(hi_filtered60) && length(hi_filtered60) == length(cutrial_no_filtered60)) {
     plot(-1000, 1000, type = "n",
          main = "Aim Deviation Across All Trials", 
-         xlab = "Trial", ylab = "Aiming Startegy (degrees)",
-         xlim = c(0, 256), ylim = c(-20, 60))
+         xlab = "Trial", ylab = "Aiming Strategy (degrees)",
+         xlim = c(0, 256), ylim = c(-20, 70))
     
     #lines(all_data$Group.1, all_data$x, col = "hotpink", lwd = 2, pch = 16)  
-    all_data_clean <- all_data[!is.na(all_data$Group.1) & !is.na(all_data$x) & 
-                                 !is.infinite(all_data$Group.1) & !is.infinite(all_data$x), ]
+    all_data_clean60 <- all_data60[!is.na(all_data60$Group.1) & !is.na(all_data60$x) & 
+                                     !is.infinite(all_data60$Group.1) & !is.infinite(all_data60$x), ]
     
-
-    smoothall <- smooth.spline(all_data_clean$Group.1, all_data_clean$x, spar = 0.0001)
-    lines(smoothall, col='magenta4', lwd=2)
+    
+    smoothall60 <- smooth.spline(all_data_clean60$Group.1, all_data_clean60$x, spar = 0.0001)
+    lines(smoothall60, col='magenta4', lwd=2)
     abline(h = 0, col = "black", lty = 3)
     
-    lo_smooth_filtered <- smooth.spline(cutrial_no_filtered, lo_filtered, spar = 0.008)
-    hi_smooth_filtered <- smooth.spline(cutrial_no_filtered, hi_filtered, spar = 0.008)
+    lo_smooth_filtered60 <- smooth.spline(cutrial_no_filtered60, lo_filtered60, spar = 0.008)
+    hi_smooth_filtered60 <- smooth.spline(cutrial_no_filtered60, hi_filtered60, spar = 0.008)
     
     # Draw the polygon for the filtered CIs
-    polygon(x = c(lo_smooth_filtered$x, rev(hi_smooth_filtered$x)),
-            y = c(lo_smooth_filtered$y, rev(hi_smooth_filtered$y)),
+    polygon(x = c(lo_smooth_filtered60$x, rev(hi_smooth_filtered60$x)),
+            y = c(lo_smooth_filtered60$y, rev(hi_smooth_filtered60$y)),
             col =rgb(0.6, 0, 0.6, alpha = 0.2) ,
             border = NA)
   } else {
     stop("Lengths of lo_filtered, hi_filtered, and cutrial_no_filtered do not match!")
   }
-}
-}
-
-
-
-
-
